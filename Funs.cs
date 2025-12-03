bc47b0635d0858a8990a4b1c583e68dc95de9dbc
Палкина Софья Андреевна <kseniashap0va98@gmail.com>
2025-12-03 11:43:50 +0500

    Update Funs.cs

--- Funs.cs
+++ Funs.cs
@@ -20,5 +20,19 @@ namespace C_Sharp_lab_6.Ex1
                 meowable.meow();
             }
         }
+
+        /// <summary>
+        /// Метод для последовательного мяуканья times раз одного и более мяукающих объектов
+        /// </summary>
+        /// <param name="times">Количество мяуканий</param>
+        /// <param name="meowables">Мяукающие объекты</param>
+        public static void meowCare(int times, params Meowable[] meowables)
+        {
+            foreach (var meowable in meowables)
+            {
+                meowable.meow(times);
+            }
+        }
     }
 }
+
